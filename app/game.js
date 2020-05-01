b9ccaf9c8f8e1fd41b8beda8944d5eaca5928709
Dylan Dawkins <dad586@nyu.edu>
2020-04-30 21:11:57 -0400

    Change Scale

--- app/game.js
+++ app/game.js
@@ -106,7 +106,7 @@ module.exports = class Game extends EventEmitter {
 
     scaleEllipse(event){
 
-        ellipseScale = clamp(ellipseScale * event.scale, 0, 4);
+        ellipseScale = clamp(ellipseScale + event.scale -1, 0, 4);
         
     }
 
